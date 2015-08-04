4b3e6b8abe9b00794baf03aaa64a81bce6e52901
Bryan <me>
2015-08-04 08:48:53 -0500

    1st commit

---
+++ src/main/java/lounge/GradleLoungeApplication.java
@@ -0,0 +1,12 @@
+package lounge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleLoungeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleLoungeApplication.class, args);
+    }
+}
